2d06f6ee0adc30441ab21c70b2805088140e1d30
mohirwanh <mohirwanh@gmail.com>
2023-03-01 21:30:04 +0700

    apps

--- src/app.ts
+++ src/app.ts
@@ -12,15 +12,44 @@ interface Product {
 function getProduct(id: number): Product {
     return {
         id: id,
-        name: `Awesome Gadget ${id}`,
+        name: `Awesome Device ${id}`,
         price: 99.5
     }
 }
 
+//GET Example
 app.get("/", (req: Request, res: Response): void => {
     const product = getProduct(1);
     res.send(product);
 });
+
+// POST method route
+app.post('/', (req, res) => {
+    res.send('POST request to the homepage')
+})
+
+// Response example
+app.get("/json/:id", (req: Request, res: Response): void => {
+    const product = getProduct(Number(req.params['id']));
+    res.json(product);
+});
+
+app.get("/jsonp/:id", (req: Request, res: Response): void => {
+    const product = getProduct(Number(req.params['id']));
+    res.jsonp(product);
+});
+
+//Route
+app.route('/book')
+    .get((req, res) => {
+        res.send('Get a random book')
+    })
+    .post((req, res) => {
+        res.send('Add a book')
+    })
+    .put((req, res) => {
+        res.send('Update the book')
+    })
 
 app.listen(PORT, (): void => {
     console.log(`Server Running here 👉 https://localhost:${PORT}`);
